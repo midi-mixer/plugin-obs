ca6a8a1421207adf2bcec4e293b087c5d805b703
Jack Williams <jpwilliamsphotography@gmail.com>
2021-05-02 17:55:37 +0100

    chore: add node polyfills for EventEmitter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // public
 import { defineConfig } from "vite";
 import copy from "rollup-plugin-copy";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
   base: "./",
@@ -21,5 +22,6 @@ export default defineConfig({
       ],
       hook: "writeBundle",
     }),
+    nodePolyfills(),
   ],
 });
